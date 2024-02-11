b472dcaad44783b5da34374d1f97dd8ec695bc5d
John Arocha Dev <jparocha777@gmail.com>
2024-02-11 12:20:54 -0500

    added tabs to applied jobs

--- src/components/Applied/Applied.jsx
+++ src/components/Applied/Applied.jsx
@@ -1,9 +1,29 @@
 import { Container } from "react-bootstrap"
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 export default function Applied() {
   return (
     <Container>
-      <h1>Applied</h1>
+      <Tabs
+        defaultActiveKey="profile"
+        id="fill-tab-example"
+        className="mb-3"
+        fill
+      >
+        <Tab eventKey="saved" title="Saved">
+          Tab content for Home
+        </Tab>
+        <Tab eventKey="applied" title="Applied">
+          Tab content for Profile
+        </Tab>
+        <Tab eventKey="interview" title="Interview">
+          Tab content for Loooonger Tab
+        </Tab>
+        <Tab eventKey="rejected" title="Rejected" >
+          Tab content for Contact
+        </Tab>
+      </Tabs>
     </Container>
   )
 }
