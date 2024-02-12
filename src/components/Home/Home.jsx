1464fc1562fd761560c549d1ff700ca9fb8373d7
John Arocha Dev <jparocha777@gmail.com>
2024-02-12 13:45:21 -0500

    added logic to make api call from home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,16 @@
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import InputGroup from 'react-bootstrap/InputGroup';
+import { useState } from 'react'
 import './Home.css'
 
 export default function Home({ msgAlert, user, query, setQuery, location, setLocation } ) {
+	const [userLocation, setUserLocation] = useState('')
+	const [userJob, setUserJob] = useState('')
 
 	function handleSubmit(e) {
 		e.preventDefault()
-		console.log('job', query)
-		console.log('location', location)
+		setLocation(userLocation)
+		setQuery(userJob)
 	}
 
 	return (
@@ -16,8 +19,8 @@ export default function Home({ msgAlert, user, query, setQuery, location, setLoc
 					<Col style={{ display: 'flex', alignItems: 'center' }}>
 							<Form>
 									<InputGroup className="mb-3">
-										<Form.Control type="text" placeholder="Search Jobs" id="jobSearch" onChange={e => setQuery(e.target.value)} />
-										<Form.Control type="text" placeholder="Location" id="locationSearch" onChange={e => setLocation(e.target.value)} />
+										<Form.Control type="text" placeholder="Search Jobs" id="jobSearch" onChange={e => setUserJob(e.target.value)} />
+										<Form.Control type="text" placeholder="Location" id="locationSearch" onChange={e => setUserLocation(e.target.value)} />
 										<Button type="submit" style={{ marginRight: '1vh', backgroundColor: '#2AABB6', border: '#2AABB6', color: 'black'}} onClick={handleSubmit} >Submit</Button>
 									</InputGroup>
 							</Form>
@@ -26,29 +29,4 @@ export default function Home({ msgAlert, user, query, setQuery, location, setLoc
 		</Container>
 	)
 }
-
-// 	return (
-// 		<Container className="bgColor" fluid>
-//       <Row className='emptyBar'>
-//         <div className="filterButton" style={{ display: "flex", justifyContent: 'space-around' }}>
-//           <Col style={{ display: 'flex', alignItems: 'center' }}>
-//             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-//               <Form>
-//                 <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-//                   <Form.Control style={{ marginRight: '1vh', marginLeft: '17vh', width: '30vh' }} type="text" placeholder="Search Jobs" id="jobSearch" onChange={e => setUserJob(e.target.value)} />
-//                   <Form.Control style={{ marginRight: '1vh', width: '30vh' }} type="text" placeholder="Location" id="locationSearch" onChange={e => setUserLocation(e.target.value)} />
-//                   <Button type="submit" style={{ marginRight: '1vh', backgroundColor: '#2AABB6', border: '#2AABB6', color: 'black'}} onClick={handleSubmit} >Submit</Button>
-//                 </div>
-//               </Form>
-
-//               <Button onClick={() => setModalShow(true)} style={{backgroundColor: '#2AABB6', border: '#2AABB6', color: 'black'}} >
-//                 Filter
-//               </Button>
-//             </div>
-//           </Col>
-//         </div>
-//       </Row>
-// 		</Container>
-// 	)
-// }
 
