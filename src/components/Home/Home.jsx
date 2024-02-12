a3f2f17d9e8063a822496344e0acef902b762a28
John Arocha Dev <jparocha777@gmail.com>
2024-02-12 13:58:37 -0500

    new styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -22,18 +22,18 @@ export default function Home({ msgAlert, user, query, setQuery, location, setLoc
 	}
 
 	return (
-		<Container className="bgColor" fluid>
-			<Row className='emptyBar'>
-					<Col style={{ display: 'flex', alignItems: 'center' }}>
-							<Form>
-									<InputGroup className="mb-3">
-										<Form.Control type="text" placeholder="Search Jobs" id="jobSearch" onChange={e => setUserJob(e.target.value)} />
-										<Form.Control type="text" placeholder="Location" id="locationSearch" onChange={e => setUserLocation(e.target.value)} />
-										<Button type="submit" style={{ marginRight: '1vh', backgroundColor: '#2AABB6', border: '#2AABB6', color: 'black'}} onClick={handleSubmit} >Submit</Button>
-									</InputGroup>
-							</Form>
-					</Col>
-			</Row>
-		</Container>
+    <Container className="bgColor" fluid >
+      <Row className='emptyBar' style={{ minHeight: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'flex-start' }}>
+        <Col xs={12} md={6}>
+          <Form>
+            <InputGroup style={{paddingTop: '13vh'}}>
+              <Form.Control type="text" placeholder="Search Jobs" id="jobSearch" onChange={e => setUserJob(e.target.value)} />
+              <Form.Control type="text" placeholder="Location" id="locationSearch" onChange={e => setUserLocation(e.target.value)} />
+              <Button type="submit" style={{ backgroundColor: '#2AABB6', border: '#2AABB6', color: 'black' }} onClick={handleSubmit}>Submit</Button>
+            </InputGroup>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
 	)
 }
