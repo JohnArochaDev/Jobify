68d8dfca1920fd78348f302c57f380af73ef15fe
John Arocha Dev <jparocha777@gmail.com>
2024-02-12 20:54:20 -0500

    card animation lines up with original cards that will laod

--- src/components/Jobs/JobComponent/JobComponent.jsx
+++ src/components/Jobs/JobComponent/JobComponent.jsx
@@ -58,13 +58,3 @@ export default function JobComponent({setSelectedJob, job, i, user}) {
       </>
   )
 }
-
-{/* <Card style={{display: 'flex', flexDirection: 'column' }} key={iii}>
-<Card.Img className='loadingImg' src="/photos/SFLS.jpg" alt="Card image" />
-<Card.ImgOverlay>
-  <Card.Title></Card.Title>
-  <Card.Text>
-  </Card.Text>
-  <Card.Text></Card.Text>
-</Card.ImgOverlay>
-</Card> */}
--- src/components/Jobs/JobComponent/JobComponentLoad.jsx
+++ src/components/Jobs/JobComponent/JobComponentLoad.jsx
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap"
 export default function JobComponentLoad({loadAmount}) {
   return (
     loadAmount.map((job, i) => (
-    <Card style={{display: 'flex', flexDirection: 'column'}} key={i}>
+    <Card className="jobCard" style={{display: 'flex', flexDirection: 'column'}} key={i}>
       <Card.Img className='loadingImg' src="/photos/SFLS.jpg" alt="Card image" />
       <Card.ImgOverlay>
         <Card.Title></Card.Title>
