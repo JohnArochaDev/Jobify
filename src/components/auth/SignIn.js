2f015caabaff82b0fcfd998b1aac10c7516f5037
John Arocha Dev <jparocha777@gmail.com>
2024-02-13 09:33:03 -0500

    added basic styling to sign in

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -46,39 +46,69 @@ export default function SignIn(props) {
 				})
 			})
 	    }
+
+        function onSignUp(event) {
+            event.preventDefault()
+            navigate('/sign-up')
+        }
+
+
+        const containerStyle = {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 'auto',
+          };
+        
+          const roundedContainerStyle = {
+            marginTop: '20vh',
+            backgroundColor: '#fff',
+            border: '1px solid #ddd',
+            borderRadius: '10px',
+            padding: '10vh',
+            boxShadow: '0 0 20px rgba(0, 0, 0, 0.2)',
+            width: '100vh',
+          };
         
     return (
-        <div className='row'>
-            <div className='col-sm-10 col-md-8 mx-auto mt-5'>
+        <div style={containerStyle}>
+            <div className='row'>
+                <div className='col-sm-10 col-md-8' style={roundedContainerStyle}>
                 <h3>Sign In</h3>
                 <Form onSubmit={onSignIn}>
                     <Form.Group controlId='email'>
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control
-                            required
-                            type='username'
-                            name='email'
-                            value={email}
-                            placeholder='Enter email or username'
-                            onChange={e => setEmail(e.target.value)}
-                        />
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                        required
+                        type='username'
+                        name='email'
+                        value={email}
+                        placeholder='Enter email or username'
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                     </Form.Group>
                     <Form.Group controlId='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            required
-                            name='password'
-                            value={password}
-                            type='password'
-                            placeholder='Password'
-                            onChange={e => setPassword(e.target.value)}
-                        />
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        required
+                        name='password'
+                        value={password}
+                        type='password'
+                        placeholder='Password'
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                     </Form.Group>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '2vh' }}>
                     <Button variant='primary' type='submit'>
                         Submit
                     </Button>
+                    <Button variant='primary' type='submit' onClick={onSignUp} >
+                        Sign Up
+                    </Button>
+                    </div>
                 </Form>
+                </div>
             </div>
         </div>
-    )
+    );
 }
