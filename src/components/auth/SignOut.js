89ea097358566aa6999fc0f5db7ff4d4b6c03b9f
John Arocha Dev <jparocha777@gmail.com>
2024-02-13 10:09:06 -0500

    styled sign out

--- src/components/auth/SignOut.js
+++ src/components/auth/SignOut.js
@@ -28,24 +28,38 @@ const SignOut = (props) => {
         navigate('/')
     }
 
+    const roundedContainerStyle = {
+        marginTop: '20vh',
+        backgroundColor: '#fff',
+        border: '1px solid #ddd',
+        borderRadius: '10px',
+        padding: '10vh',
+        boxShadow: '0 0 20px rgba(0, 0, 0, 0.2)',
+        width: '100vh',
+      };
+
 	return (
-		<>
-            <div className='row'>
-                <div className='col-sm-10 col-md-8 mx-auto mt-5'>
-                    <h2>Are you sure you want to sign out?</h2>
-                    <small>We hate to see you go...</small><br/>
-                    <ButtonGroup>
-                        <Button variant='danger' onClick={onSignOut}>
-                            Sign Out
-                        </Button>
-                        <Button variant='warning' onClick={onCancel}>
-                            Cancel
-                        </Button>
-                    </ButtonGroup>
+    <>
+        <div className='row' style={{marginTop: '7vh'}} >
+            <div className='col-sm-10 col-md-8 mx-auto mt-5' style={roundedContainerStyle}>
+                <h2 style={{marginTop: '-2vh', marginBottom: '5vh'}} >Are you sure you want to sign out?</h2>
+                <div >
+                    <small >We hate to see you go...</small>
+                </div>
+                <br />
+                <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '2vh', marginBottom: '-4vh'}} >
+                    <Button variant='danger' onClick={onSignOut}>
+                        Sign Out
+                    </Button>
+                    <Button variant='warning' onClick={onCancel}>
+                        Cancel
+                    </Button>
                 </div>
             </div>
-		</>
+        </div>
+    </>
 	)
 }
+
 
 export default SignOut
